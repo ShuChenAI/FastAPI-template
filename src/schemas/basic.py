1c36f5def920f9f6929aa8c956a7d2db8b2ed9b4
HaoLiangWen <luisleo52655@gmail.com>
2024-11-05 17:13:09 +0800

    debug: add s3 upload

--- src/schemas/basic.py
+++ src/schemas/basic.py
@@ -8,3 +8,10 @@ class TextOnly(BaseModel):
 class Token(BaseModel):
     access_token: str = Field(default='example_token')
     token_type: str = Field(default='bearer')
+
+
+class UploadedFile(BaseModel):
+    path: str = Field(..., title="Local Path")
+    original_file_name: str = Field(..., title="Original File Name")
+    extension: str = Field(..., title="File Extension")
+    content_type: str = Field(..., title="Content Type")
