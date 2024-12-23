de6308f66ce32967117c2f457ca917218dcebb78
leo <luisleo52655@gmail.com>
2024-12-23 11:23:17 +0800

    Feat: renew

--- src/database/utils.py
+++ src/database/utils.py
@@ -7,5 +7,4 @@ from src.database.database import SessionLocal
 def add_test_data():
     db: Session = SessionLocal()
     create_user(db, "test-name", "test-username", "test-password", "test-user-id")
-
-
+    db.close()
--- src/server.py
+++ src/server.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI
+import os
+
+from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
 from starlette.requests import Request
 
+from src.dependencies.auth import get_admin_user
 from src.routers.auth import router as auth_router
 from src.schemas.basic import TextOnly
 
@@ -54,3 +57,17 @@ async def api_documentation(request: Request):
 
   </body>
 </html>""")
+
+
+if os.getenv('DEV', 'false') == 'true':
+    @app.post("/renewDB", dependencies=[Depends(get_admin_user)])
+    async def renew_database():
+        from src.database.database import TRIAL_URL, DB_NAME, engine, drop_all_tables
+        from src.database.database import create_all_tables, create_database_if_not_exists
+        from src.database.utils import add_test_data
+
+        create_database_if_not_exists(TRIAL_URL, DB_NAME)
+        drop_all_tables(engine)
+        create_all_tables(engine)
+        add_test_data()
+        return TextOnly(text="Database Renewed")
